f6b3df42ac13ae018365d06b6f35de95a45f4bff
Adam Chainz <adam@adamj.eu>
2014-01-25 17:51:12 +0000

    Solve question 3

---
+++ Q3/q3.py
@@ -0,0 +1,42 @@
+# coding=utf-8
+
+
+def count_in_sorted(item, search):
+    """
+    Takes an item and an ascendingly sorted list, and returns the number of
+    occurences of item in the list
+    """
+    index = binarysearch(item, search)
+    if index is None:
+        return 0
+
+    first = index
+    for i in range(index, -1, -1):
+        if search[i] < item:
+            break
+        first = i
+
+    last = index
+    for i in range(index, len(search)):
+        if search[i] > item:
+            break
+        last = i
+
+    return last - first + 1
+
+
+def binarysearch(item, search):
+    if len(search) == 0:
+        return None
+    elif len(search) == 1:
+        return 0 if search[0] == item else None
+
+    midpoint = len(search) / 2
+
+    if search[midpoint] == item:
+        return midpoint
+    elif search[midpoint] > item:
+        return binarysearch(item, search[:midpoint])
+    else:
+        subsearch = binarysearch(item, search[midpoint:])
+        return None if subsearch is None else midpoint + subsearch
---
+++ Q3/test_q3.py
@@ -0,0 +1,38 @@
+# coding=utf-8
+import unittest
+
+from q3 import count_in_sorted
+
+
+class Q3Tests(unittest.TestCase):
+    def test_empty(self):
+        self.assertEqual(count_in_sorted(2, []), 0)
+
+    def test_single(self):
+        self.assertEqual(count_in_sorted(2, [2]), 1)
+
+    def test_double(self):
+        self.assertEqual(count_in_sorted(2, [4, 1]), 0)
+        self.assertEqual(count_in_sorted(2, [2, 4]), 1)
+        self.assertEqual(count_in_sorted(2, [2, 2]), 2)
+
+    def test_triple(self):
+        self.assertEqual(count_in_sorted(2, [1, 4, 9]), 0)
+        self.assertEqual(count_in_sorted(2, [1, 2, 7]), 1)
+        self.assertEqual(count_in_sorted(2, [-22, 2, 2]), 2)
+
+    def test_example(self):
+        self.assertEqual(count_in_sorted(2, [1, 2, 2, 2, 3, 4]), 3)
+
+    def test_missing(self):
+        self.assertEqual(count_in_sorted(77, [1, 2, 2, 2, 3, 4]), 0)
+
+    def test_really_long(self):
+        long_list = [1, 1, 1] + [4] * 9999
+        self.assertEqual(count_in_sorted(7, long_list), 0)
+        self.assertEqual(count_in_sorted(1, long_list), 3)
+        self.assertEqual(count_in_sorted(4, long_list), 9999)
+
+
+if __name__ == '__main__':
+    unittest.main()
