4f72ab2ebe4962f9fe455d955be93531cd09478c
Adam Chainz <adam@adamj.eu>
2014-01-26 11:50:48 +0000

    Solve question 23

---
+++ q23/q23.py
@@ -0,0 +1,47 @@
+# coding=utf-8
+
+
+def maximal_submatrix_sum(matrix):
+    width = len(matrix)
+    height = len(matrix[0])
+
+    @memoize
+    def matrix_sum(i, j, w, h):
+        if w > 1:
+            the_sum = matrix_sum(i, j, w - 1, h)
+            # Add on final column
+            for y in xrange(j, j + h):
+                the_sum += matrix[i + w - 1][y]
+            return the_sum
+        elif h > 1:
+            the_sum = matrix_sum(i, j, w, h - 1)
+            # Add on final row
+            for x in xrange(i, i + w):
+                the_sum += matrix[x][j + h - 1]
+            return the_sum
+        else:
+            return matrix[i][j]
+
+    max_sum = float('-inf')
+    for w in xrange(1, width + 1):
+        for h in xrange(1, height + 1):
+            for i in xrange(width - w + 1):
+                for j in xrange(height - h + 1):
+                    # Check for sum of matrix width w, height h, at pos i,j
+                    sum_here = matrix_sum(i, j, w, h)
+                    if sum_here > max_sum:
+                        max_sum = sum_here
+
+    return max_sum
+
+
+def memoize(func):
+    results = {}
+
+    def wrapped(*args):
+        key = tuple(args)
+        if key not in results:
+            results[key] = func(*args)
+        return results[key]
+
+    return wrapped
---
+++ q23/test_q23.py
@@ -0,0 +1,40 @@
+# coding=utf-8
+import unittest
+
+from q23 import maximal_submatrix_sum
+
+
+class Q23Tests(unittest.TestCase):
+    def test_one_by_one(self):
+        self.assertEqual(maximal_submatrix_sum([[97]]), 97)
+
+    def test_two_by_two(self):
+        self.assertEqual(maximal_submatrix_sum([[97, -4], [-3, -23]]), 97)
+
+    def test_row(self):
+        self.assertEqual(maximal_submatrix_sum([[79, -4, -38]]), 79)
+
+    def test_column(self):
+        self.assertEqual(maximal_submatrix_sum([[44], [22], [-81]]), 66)
+
+    def test_example(self):
+        self.assertEqual(
+            maximal_submatrix_sum([[1, 2, 3], [-1, -2, -3], [0, 0, 0]]),
+            6
+        )
+
+    def test_example2(self):
+        self.assertEqual(
+            maximal_submatrix_sum([[1, -1, 0], [2, -2, 0], [3, -3, 0]]),
+            6
+        )
+
+    def test_giant(self):
+        self.assertEqual(
+            maximal_submatrix_sum([range(99), range(-99, 0)]),
+            sum(xrange(99))
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
