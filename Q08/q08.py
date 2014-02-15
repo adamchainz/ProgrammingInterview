19e42469a97bcabf02809fbb66e537185420195b
Adam Chainz <adam@adamj.eu>
2014-02-15 17:46:47 +0000

    Solve question 8

---
+++ Q08/q08.py
@@ -0,0 +1,23 @@
+class TreeNode(object):
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+
+def validate_is_search_tree(node):
+    is_valid = True
+
+    if node.left is not None:
+        is_valid |= (
+            node.value >= node.left.value and
+            validate_is_search_tree(node.left)
+        )
+
+    if node.right is not None:
+        is_valid |= (
+            node.value < node.right.value and
+            validate_is_search_tree(node.right)
+        )
+
+    return is_valid
---
+++ Q08/test_q08.py
@@ -0,0 +1,30 @@
+import unittest
+from q08 import TreeNode, validate_is_search_tree
+
+
+class ValidateTests(unittest.TestCase):
+    def test_simple_yes(self):
+        self.assertTrue(validate_is_search_tree(
+            TreeNode(5)
+        ))
+
+    def test_simple_no(self):
+        self.assertTrue(validate_is_search_tree(
+            TreeNode(
+                5,
+                left=TreeNode(8)
+            )
+        ))
+
+    def test_bigger_yes(self):
+        self.assertTrue(validate_is_search_tree(
+            TreeNode(
+                3,
+                left=TreeNode(2, left=TreeNode(1)),
+                right=TreeNode(5, left=TreeNode(4), right=TreeNode(6))
+            )
+        ))
+
+
+if __name__ == '__main__':
+    unittest.main()
