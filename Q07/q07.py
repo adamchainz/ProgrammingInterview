791d71098c689956775c95842f242d37ce1d504a
Adam Chainz <adam@adamj.eu>
2014-03-24 11:28:25 +0000

    Improve question 7 solution with better default return value and call signature

--- Q07/q07.py
+++ Q07/q07.py
@@ -1,15 +1,20 @@
 # coding=utf-8
 
 
-def first_index(numbers, value, start, end):
-    if end < start or value < numbers[start] or value > numbers[end]:
-        return -1
+def first_index(numbers, value, start=None, end=None):
+    if start is None:
+        start = 0
+    if end is None:
+        end = len(numbers) - 1
+
+    if value < numbers[start] or value > numbers[end]:
+        return None
+
+    midpoint = (start + end) / 2
+    if numbers[midpoint] > value or (midpoint - 1 >= 0 and
+                                     numbers[midpoint - 1] == value):
+        return first_index(numbers, value, start, midpoint - 1)
+    elif numbers[midpoint] < value:
+        return first_index(numbers, value, midpoint + 1, end)
     else:
-        midpoint = (start + end) / 2
-        if numbers[midpoint] > value or (midpoint-1 >= 0 and 
-                                         numbers[midpoint-1] == value):
-            return first_index(numbers, value, start, midpoint-1)
-        elif numbers[midpoint] < value:
-            return first_index(numbers, value, midpoint+1, end)
-        else:
-            return midpoint
+        return midpoint
--- Q07/test_q07.py
+++ Q07/test_q07.py
@@ -6,20 +6,19 @@ from q07 import first_index
 
 class Q7Tests(unittest.TestCase):
     def test_no_duplicates(self):
-        numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-        self.assertEqual(first_index(numbers, 3, 0, len(numbers)-1), 2)
+        self.assertEqual(first_index([1, 2, 3, 4, 5, 6, 7, 8, 9], 3), 2)
 
     def test_some_duplicates(self):
         numbers = [1, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 4, 5, 6, 7, 8, 9]
-        self.assertEqual(first_index(numbers, 3, 0, len(numbers)-1), 3)
+        self.assertEqual(first_index(numbers, 3), 3)
 
     def test_all_duplicates(self):
         numbers = [3, 3, 3, 3, 3, 3, 3, 3, 3]
-        self.assertEqual(first_index(numbers, 3, 0, len(numbers)-1), 0)
+        self.assertEqual(first_index(numbers, 3), 0)
 
     def test_not_found(self):
         numbers = [1, 2, 4, 5, 6, 7, 8, 9]
-        self.assertEqual(first_index(numbers, 3, 0, len(numbers)-1), -1)
+        self.assertEqual(first_index(numbers, 3), None)
 
 
 if __name__ == '__main__':
