11a9b2caee541af5d67d80ce53e81e6c5bf58e26
Adam Chainz <adam@adamj.eu>
2014-03-26 12:23:57 +0000

    Implement recursive solution for question 11 as well

--- Q11/q11.py
+++ Q11/q11.py
@@ -12,3 +12,20 @@ def subsets(elements):
         results.add(tuple(result))
 
     return results
+
+
+def subsets_recursive(elements):
+    if len(elements) == 0:
+        return set([()])
+
+    this, others = elements[0], elements[1:]
+    results = set()
+    sub_results = subsets_recursive(others)
+
+    # All possible subsets = all possible subsets of 'others',
+    # without or with 'this' element
+    results.update(sub_results)
+    for sub_result in sub_results:
+        results.add((this,) + sub_result)
+
+    return results
--- Q11/test_q11.py
+++ Q11/test_q11.py
@@ -1,33 +1,42 @@
 # coding=utf-8
 import unittest
 
-from q11 import subsets
+from q11 import subsets, subsets_recursive
 
 
-class Q11Tests(unittest.TestCase):
+class Q11TestsMixin(object):
     def test_zero(self):
         self.assertSetEqual(
-            subsets([]),
+            self.check_func([]),
             set([()])
         )
 
     def test_single(self):
         self.assertSetEqual(
-            subsets([1]),
+            self.check_func([1]),
             set([(), (1,)])
         )
 
     def test_two(self):
         self.assertSetEqual(
-            subsets([1, 2]),
+            self.check_func([1, 2]),
             set([(), (1,), (2,), (1, 2)])
         )
 
     def test_three(self):
         self.assertSetEqual(
-            subsets([1, 2, 3]),
+            self.check_func([1, 2, 3]),
             set([(), (1,), (2,), (3,), (1, 2), (1, 3), (2, 3), (1, 2, 3)])
         )
+
+
+class Q11NonrecursiveTests(Q11TestsMixin, unittest.TestCase):
+    check_func = staticmethod(subsets)
+
+
+class Q11RecursiveTests(Q11TestsMixin, unittest.TestCase):
+    check_func = staticmethod(subsets_recursive)
+
 
 if __name__ == '__main__':
     unittest.main()
